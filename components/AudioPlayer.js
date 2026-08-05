e1870e3a5f73e3b164df9fe55247d3c4b9507aeb
denis-apolinarioo <denislacerda.ilovegod@gmail.com>
2026-08-05 00:30:31 -0300

    v1

--- components/AudioPlayer.js
+++ components/AudioPlayer.js
@@ -115,7 +115,6 @@ export default function AudioPlayer({ src, onTap, className = '' }) {
       <audio
         ref={audioRef}
         src={src}
-        crossOrigin="anonymous"
         onTimeUpdate={handleTimeUpdate}
         onLoadedMetadata={handleLoadedMetadata}
         onEnded={handleEnded}
