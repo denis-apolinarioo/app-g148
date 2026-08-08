cf6e71ad6b5dbd6f7464b8efc0f50db42f15ed39
denis-apolinarioo <denislacerda.ilovegod@gmail.com>
2026-08-07 21:45:59 -0300

    Delete components directory

--- components/LoadingScreen.js
+++
@@ -1,8 +0,0 @@
-export default function LoadingScreen({ mensagem = 'Carregando...' }) {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-4 bg-cream">
-      <div className="h-10 w-10 animate-spin rounded-full border-[3px] border-coffee-200 border-t-coffee-600" />
-      <p className="font-body text-sm text-coffee-400">{mensagem}</p>
-    </div>
-  );
-}
